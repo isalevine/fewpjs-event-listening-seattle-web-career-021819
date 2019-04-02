6438b1fae380ab4f627c0e6e6b4d8d7b8b09efc6
github username <github email address>
2019-04-01 17:06:40 -0700

    fixing inconsistencies

--- index.js
+++ index.js
@@ -2,9 +2,16 @@ const input = document.querySelector('input');
 
 function addingEventListener() {
   input.addEventListener('click', function(event) {
-    return "clicked"
+    alert("clicked")
   });
 }
+addingEventListener();
+
+const main = document.getElementById('main');
+ 
+main.addEventListener('click', function(event) {
+  alert('I was clicked!');
+});
 
 function preventingDefault() {
   input.addEventListener('keydown', function(e) {
